57d9a7b6b2f2452ad79d2cf914891f8b26eafa7e
Kashish <98416263+KashishSharma8214@users.noreply.github.com>
2026-06-01 20:02:45 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String,List<String>> h1 = new HashMap<>();
+        for(int i = 0 ; i<strs.length;i++){  
+            char[] c1 = strs[i].toCharArray();
+            Arrays.sort(c1);
+            //Convert sorted character array back  to string 
+            String Key = new String(c1);
+            h1.putIfAbsent(Key, new ArrayList<>());
+            h1.get(Key).add(strs[i]);
+            
+
+
+        }
+        return new ArrayList<>(h1.values());
+        //TE - O(n * m log m)
+        
+    }
+}   
+ 
